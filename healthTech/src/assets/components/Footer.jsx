f9ca5398511792fc6b62009ace2dde28a129405c
Leandro Simoneli <simonelileandro437@gmail.com>
2025-11-16 16:11:12 -0300

    feat: criar componente Footer

---
+++ healthTech/src/assets/components/Footer.jsx
@@ -0,0 +1,11 @@
+// src/components/Footer.jsx
+function Footer() {
+  return (
+    <footer className="border-t border-slate-200 dark:border-slate-800 py-4 text-center text-xs text-slate-500 dark:text-slate-400">
+      Global Solution · Front-End Web Development · 2º semestre/2025 · Foco em
+      Saúde
+    </footer>
+  );
+}
+
+export default Footer;
